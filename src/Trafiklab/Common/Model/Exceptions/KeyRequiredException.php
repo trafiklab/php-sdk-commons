54d619d6948b74431039f3c24a6fa08e10c5c396
Bert <bert.marcelis@samtrafiken.se>
2019-05-03 10:11:22 +0200

    Small bugfix

--- src/Trafiklab/Common/Model/Exceptions/KeyRequiredException.php
+++ src/Trafiklab/Common/Model/Exceptions/KeyRequiredException.php
@@ -12,7 +12,7 @@ class KeyRequiredException extends InvalidRequestException
 {
     public function __construct(string $detail = "")
     {
-        parent::__construct("An API key is required to make this request. $detail", null);
+        parent::__construct("An API key is required to make this request. $detail");
         $this->code = 401;
     }
 }
