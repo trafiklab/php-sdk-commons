1a7fd34c72e9050252668b155998557e9516a040
Bert <bert.marcelis@samtrafiken.se>
2019-05-07 18:14:56 +0200

    Small fixes and documentation

--- src/Trafiklab/Common/Model/Contract/RoutePlanningRequest.php
+++ src/Trafiklab/Common/Model/Contract/RoutePlanningRequest.php
@@ -4,6 +4,7 @@ namespace Trafiklab\Common\Model\Contract;
 
 
 use DateTime;
+use Trafiklab\Common\Model\Enum\RoutePlanningSearchType;
 
 /**
  * A request for a route-planning. Implementing classes may provide more detailed functionality.
@@ -25,9 +26,23 @@ interface RoutePlanningRequest
 
     public function setDestinationStopId(string $stopId): void;
 
+    /**
+     * Get the type of time definition in this query. Either Arriving at a certain time, or departing at a certain time.
+     *
+     * @return int One of the constants defined in RoutePlanningSearchType
+     *
+     * @see RoutePlanningSearchType
+     */
     public function getRoutePlanningSearchType(): int;
 
-    public function setRoutePlanningSearchType(int $timeTableType): void;
+    /**
+     * Set the type of time definition in this query. Either Arriving at a certain time, or departing at a certain time.
+     *
+     * @param int $routePlanningSearchType One of the constants defined in RoutePlanningSearchType
+     *
+     * @see RoutePlanningSearchType
+     */
+    public function setRoutePlanningSearchType(int $routePlanningSearchType): void;
 
     public function getDateTime(): DateTime;
 
--- src/Trafiklab/Common/Model/Contract/TimeTableRequest.php
+++ src/Trafiklab/Common/Model/Contract/TimeTableRequest.php
@@ -4,6 +4,7 @@ namespace Trafiklab\Common\Model\Contract;
 
 
 use DateTime;
+use Trafiklab\Common\Model\Enum\TimeTableType;
 
 /**
  * A request for a timetable. Implementing classes may provide more detailed functionality.
@@ -18,11 +19,27 @@ interface TimeTableRequest
 
     public function setStopId(string $stopId): void;
 
+    /**
+     * Get the type of the TimeTable, either the constant defined in TimeTableType for departures or for arrivals.
+     *
+     * @see TimeTableType
+     *
+     * @return int $timeTableType the type of the TimeTable, either the constant defined in TimeTableType
+     *                            for departures or for arrivals.
+     */
     public function getTimeTableType(): int;
 
+    /**
+     * Set the type of the TimeTable, either the constant defined in TimeTableType for departures or for arrivals.
+     *
+     * @see TimeTableType
+     *
+     * @param int $timeTableType the type of the TimeTable, either the constant defined in TimeTableType
+     *                           for departures or for arrivals.
+     */
     public function setTimeTableType(int $timeTableType): void;
 
-    public function getDateTime(): ?DateTime;
+    public function getDateTime(): DateTime;
 
     public function setDateTime(?DateTime $timeTableType): void;
 }
